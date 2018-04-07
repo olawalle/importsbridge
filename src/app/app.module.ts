3e45dac4868e2cef86d3bc3b32934de548be0b81
walle <olawalle94@gmail.com>
2018-04-07 11:51:42 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent
   ],
   imports: [
     BrowserModule
